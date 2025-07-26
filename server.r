6da8fcf7e34d0302689de472e672200a1ed44e5e
mmcgov <mattmcgovern@gmail.com>
2025-07-26 12:11:02 +0100

    removing unneeded text

--- server.r
+++ server.r
@@ -33,87 +33,7 @@ function(input, output) {
 
 
 
- # summarise(Count=(formattable::number(sum(TALLPUP_ALEV_1618),0)))%>%
-  
-# output$School_Proportion <- renderTable({
-#   alc %>%
-#     group_by(SCHOOLTYPE) %>%
-#     summarise(Count=(sum(TALLPUP_ALEV_1618)))%>%
-#     mutate(Frequency = paste0(formattable::percent(Count / sum(alc$TALLPUP_ALEV_1618), 1), ""))%>%
-#     arrange(desc(Count))
-# 
-# }
-# , width = "75%"
-# ,striped = TRUE,
-# hover = TRUE
-# )
-
-# 
-#   output$School_Proportion <- renderTable({
-#     alc %>%
-#       group_by(SCHOOLTYPE) %>%
-#       summarise(Count = sum(TALLPUP_ALEV_1618)) %>%
-#       mutate(
-#         Count = as.integer(Count), # Convert Count to integers
-#         Proportion = paste0(formattable::percent(Count / sum(alc$TALLPUP_ALEV_1618), 1), "")
-#       ) %>%
-#       arrange(desc(Count))
-#   }, 
-#   width = "75%", 
-#   striped = TRUE, 
-#   hover = TRUE)
-#   
-#   output$schtyp <- renderTable({
-#     super_sleepers  
-#   })
-
-
-
-
-# 
-# output$bell<-renderPlot({ 
-#   options(repr.plot.width = 10, repr.plot.height = 10)
-#   ggplot(alc, aes(x = TALLPPE_ALEV_1618)) +
-#     geom_histogram(fill = "white", colour = "black", bins=as.numeric(input$n_breaks)) +
-#     ggtitle("Mean and Standard Deviation of TALLPPE_ALEV_1618") +
-#     
-#     theme (axis.text=element_text(size=15)) +
-#     ylab("Count of Schools")+  xlab("TALLPPE_ALEV_1618") +
-#     theme(plot.title = element_text(hjust = 0.5)) +
-#     theme (plot.title = element_text(size = 100)) +
-#     theme_grey(base_size = 15)+
-#     theme(axis.text.x= element_text(size = 15),axis.text.y= element_text(size = 15))+
-#     geom_vline(xintercept = mean(alc$'TALLPPE_ALEV_1618'), linetype="dashed", color = "blue")+
-#     annotate("text", x= mean(alc$'TALLPPE_ALEV_1618'), y=325, label="Mean 31.8", angle=0)+
-#     geom_vline(xintercept = (mean(alc$'TALLPPE_ALEV_1618')-sd(alc$'TALLPPE_ALEV_1618')), linetype="dashed", color = "red")+
-#     annotate("text", x =(mean(alc$'TALLPPE_ALEV_1618')-sd(alc$'TALLPPE_ALEV_1618')), y=325, label="Lower SD 27.4", angle=0)+
-#     geom_vline(xintercept = (mean(alc$'TALLPPE_ALEV_1618')+sd(alc$'TALLPPE_ALEV_1618')), linetype="dashed", color = "red")+
-#     annotate("text", x =(mean(alc$'TALLPPE_ALEV_1618')+sd(alc$'TALLPPE_ALEV_1618')), y=325, label="Upper SD 35.9", angle=0)
-# })
-# 
-#                                                                                    
-
-# output$"bellST"<-renderPlot({ 
-#   options(repr.plot.width = 10, repr.plot.height = 10)
-#   fil_alc <- alc %>% 
-#     filter(SCHOOLTYPE == input$selected_category)
-#   ggplot(fil_alc, aes(x = TALLPPE_ALEV_1618)) +
-#     geom_histogram(fill = "white", colour = "black", bins=as.numeric(input$n_breaksST)) +
-#     ggtitle("Distribution of TALLPPE_ALEV_1618") +
-#     labs(y="Count of Schools",size = 100)+
-#     theme(axis.text.x= element_text(size = 25),axis.text.y= element_text(size = 25))+
-#   theme_grey(base_size = 15)
-  # +
-  #   labs(y="Count")+ 
-  #   theme(plot.title = element_text(hjust = 0.5)) +
-  #   theme (plot.title = element_text(size = 17)) +
-  #   geom_vline(xintercept = mean(alc$'TALLPPE_ALEV_1618'), linetype="dashed", color = "blue")+
-  #   annotate("text", x= mean(alc$'TALLPPE_ALEV_1618'), y=325, label="Mean", angle=0)+
-  #   geom_vline(xintercept = (mean(alc$'TALLPPE_ALEV_1618')-sd(alc$'TALLPPE_ALEV_1618')), linetype="dashed", color = "red")+
-  #   annotate("text", x =(mean(alc$'TALLPPE_ALEV_1618')-sd(alc$'TALLPPE_ALEV_1618')), y=325, label="Lower SD", angle=0)+
-  #   geom_vline(xintercept = (mean(alc$'TALLPPE_ALEV_1618')+sd(alc$'TALLPPE_ALEV_1618')), linetype="dashed", color = "red")+
-  #   annotate("text", x =(mean(alc$'TALLPPE_ALEV_1618')+sd(alc$'TALLPPE_ALEV_1618')), y=325, label="Upper SD", angle=0)
-# })
+                                                                                 
 
 
 
@@ -159,9 +79,7 @@ alc2122<-read_csv("./england_ks5final2122.csv", na = c('*','','na')) %>%
 final<-left_join(alc,(alc1819 %>% select(URN,'18/19','18/19 Grade')), by = "URN") %>% 
   left_join(.,(alc2122 %>% select(URN,'21/22','21/22 Grade')), by = "URN")
 
-# finalt<-final %>% select(final$`18/19`) %>% summarise()
-#     
-#   final %>% summarise()
+
 
 summarise(final)
 
